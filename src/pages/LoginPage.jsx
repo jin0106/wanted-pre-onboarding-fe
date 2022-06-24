ee10e0321809aef3e93d7cea01182a98b7fa7160
우동진 <wjinh93@gmail.com>
2022-06-24 17:10:21 +0900

    feat : LoginPage UI 구현

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <Form>
+      <Input placeholder="Type your id" />
+      <Input type="password" placeholder="Type your password" />
+      <Button disabled>Login</Button>
+    </Form>
+  );
 }
 
 export default LoginPage;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
