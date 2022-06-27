a4d744cc918f8acf9d02c010100b91283f2f0705
우동진 <wjinh93@gmail.com>
2022-06-27 17:44:47 +0900

    feat: validation check 함수 구현

---
+++ src/utils/checkValidation.js
@@ -0,0 +1,10 @@
+export const checkEmailValidation = (email) => {
+  const reg =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  return reg.test(email);
+};
+
+export const checkPasswordValidation = (password) => {
+  const reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+  return reg.test(password);
+};
