c5e384d2ed93e1542f27d2de8dffac7a8483ce92
우동진 <wjinh93@gmail.com>
2022-06-27 17:44:35 +0900

    feat: validation 함수 구현

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import {
+  checkEmailValidation,
+  checkPasswordValidation,
+} from '../utils/checkValidation';
 function LoginPage() {
   const [loginInfo, setLoginInfo] = useState({ email: '', password: '' });
+  const [validation, setValidation] = useState({ email: true, password: true });
 
   const changeHandler = (e) => {
     const { id, value } = e.target;
     setLoginInfo({ ...loginInfo, [id]: value });
+  };
+
+  const checkValidation = (e) => {
+    const { id, value } = e.target;
+    if (id === 'email')
+      return setValidation({
+        ...validation,
+        [id]: checkEmailValidation(value),
+      });
+    if (id === 'password')
+      return setValidation({
+        ...validation,
+        [id]: checkPasswordValidation(value),
+      });
   };
 
   return (
@@ -15,12 +33,16 @@ function LoginPage() {
         id="email"
         placeholder="Type your email"
         onChange={changeHandler}
+        onBlur={checkValidation}
+        validation={validation.email}
       />
       <Input
         type="password"
         id="password"
         placeholder="Type your password"
         onChange={changeHandler}
+        onBlur={checkValidation}
+        validation={validation.password}
       />
       <Button disabled>Login</Button>
     </Form>
@@ -31,6 +53,9 @@ export default LoginPage;
 
 const Form = styled.form``;
 
-const Input = styled.input``;
+const Input = styled.input`
+  border: 1px solid;
+  border-color: ${({ validation }) => (validation ? 'black' : 'red')};
+`;
 
 const Button = styled.button``;
