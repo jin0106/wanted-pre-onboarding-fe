2d93eb1d52c4bc7a4b3042e9a2a8e76d7f1597b5
우동진 <wjinh93@gmail.com>
2022-06-29 00:59:46 +0900

    feat: Feed 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import Feed from './components/Feed';
 import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   const isLogin = localStorage.getItem('userInfo');
 
-  return isLogin ? <Navbar /> : <LoginPage />;
+  return isLogin ? (
+    <>
+      <Navbar />
+      <Feed />
+    </>
+  ) : (
+    <LoginPage />
+  );
 }
 
 export default App;
