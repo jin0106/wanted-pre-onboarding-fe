ee91516e126a8ea298007c5a13cb62f0b076f0e7
우동진 <wjinh93@gmail.com>
2022-06-28 23:39:13 +0900

    feat: theme 파일 생성 및 global 적용

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles />
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+const size = {
+  tabletS: '1060px',
+  mobile: '790px',
+  mobileS: '600px',
+  mobileXS: '450px',
+};
+
+const theme = {
+  tabletS: `(max-width: ${size.tabletS})`,
+  mobile: `(max-width: ${size.mobile})`,
+  mobileS: `(max-width:${size.mobileS})`,
+  mobileXS: `(max-width:${size.mobileXS})`,
+};
+
+export default theme;
