78cdc3946c11e1809c8f207279643df1ccbe98d3
우동진 <wjinh93@gmail.com>
2022-06-28 23:16:25 +0900

    feat: Navbar UI 구현

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  faHouse,
+  faInbox,
+  faHeart,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Navbar() {
+  return (
+    <Header>
+      <MainContainer>
+        <Container>
+          <LogoImg src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png" />
+          <Search placeholder="Search" />
+          <IconContainer>
+            <Icon icon={faHouse} />
+            <Icon icon={faInbox} />
+            <Icon icon={faHeart} />
+            <Icon icon={faArrowRightFromBracket} />
+          </IconContainer>
+        </Container>
+      </MainContainer>
+    </Header>
+  );
+}
+
+export default Navbar;
+
+const Header = styled.nav`
+  position: sticky;
+  width: 100%;
+  height: 3rem;
+  background-color: #fff;
+  border-top: 1px solid rgb(219, 219, 219);
+  border-bottom: 1px solid rgba(219, 219, 219);
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80rem;
+`;
+
+const IconContainer = styled.div``;
+
+const Icon = styled(FontAwesomeIcon)`
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-left: 1.3rem;
+  cursor: pointer;
+`;
+
+const LogoImg = styled.img`
+  width: 5rem;
+  height: 2.5rem;
+`;
+
+const Search = styled.input`
+  height: 2rem;
+  width: 13rem;
+  border-radius: 0.5rem;
+  background-color: rgb(239, 239, 239);
+  padding-left: 0.5rem;
+`;
